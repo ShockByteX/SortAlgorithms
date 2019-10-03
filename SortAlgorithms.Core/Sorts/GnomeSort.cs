11e3571ad6942ffab6fe85c14e8dd9004d9a0c9f
Zer0neX <Zer0neX@protonmail.com>
2019-10-03 06:49:06 +0200

    Implemented Gnome-Sort.

---
+++ SortAlgorithms.Core/Sorts/GnomeSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortAlgorithms.Core.Sorts
+{
+    public class GnomeSort<T> : SortBase<T> where T : IComparable
+    {
+        public GnomeSort() { }
+        public GnomeSort(IEnumerable<T> items) : base(items) { }
+        protected override void DoSort()
+        {
+            int length = _array.Length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                while (i >= 0 && Compare(i, i + 1) == 1)
+                {
+                    Swap(i, i + 1);
+                    i--;
+                }
+            }
+        }
+    }
+}
